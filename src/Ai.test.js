b8a0759071016e2cc1bf608f387aa81aac1a5c05
Pavel Egorov <xoposhiy@mail.ru>
2018-03-02 10:47:21 +0500

    Инструкция в Readme как донастроить npm-скрипты для работы на windows. Поднял тесты :)

--- src/Ai.test.js
+++ src/Ai.test.js
@@ -6,12 +6,12 @@ import Countdown from './Countdown'
 const testCase = {
     initData: "init|data",
     input: "some|input|",
-    expectedOutput: "expectedOutput"
+    expectedOutput: "Command"
 }
 
 test('Ai does what is expected', t => {
     const ai = new Ai()
-    const nextMove = ai.getNextMove(StateReader.read(testCase.initData, testCase.input), new Countdown(50))
+    const nextMove = ai.getNextMove(StateReader.read(testCase.initData, testCase.input), new Countdown(5))
     // t.log(nextMove)
     t.is(nextMove, testCase.expectedOutput)
 })
