71bad4239378d94cb83e3390851e331b4802645c
Igor Lukanin <mail@igor.lukanin.name>
2018-03-02 11:41:00 +0500

    Don't transpile main bundle from ES6 to ES5

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,7 @@
-import babel from 'rollup-plugin-babel'
-
 export default {
     input: 'src/index.js',
     output: {
         file: 'bundle.js',
         format: 'iife'
-    },
-    plugins: [
-        babel({
-            exclude: 'node_modules/**'
-        })
-    ]
+    }
 }
